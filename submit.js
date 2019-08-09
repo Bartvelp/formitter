819037695a5b0192554bc4cb2035703aafdc09cc
Bart Grosman <grosman.bart@gmail.com>
2019-08-09 14:56:00 +0200

    Updated submitter

--- submit.js
+++ submit.js
@@ -1,16 +1,41 @@
-const chrome = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
+const chrome = require('chrome-aws-lambda')
+const puppeteer = require('puppeteer-core')
 
 module.exports = async (req, res) => {
+  const requestStatus = checkRequest(req)
+  if (requestStatus !== 'valid') return res.json(requestStatus)
+  const formid = req.query.formid
+  const formUrl = `https://docs.google.com/forms/d/e/${formid}/viewform?usp=sf_link`
+  console.log(`Filling form from url: ${formUrl}`)
+
   const browser = await puppeteer.launch({
     args: chrome.args,
     executablePath: await chrome.executablePath,
     headless: chrome.headless,
   });
-  
-  res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies
-  })
+  const page = await browser.newPage();
+  await page.goto(formUrl);
+  const inputEls = await page.$$eval('input[type=text]', inputs => {
+    return inputs
+  });
+  console.log(inputEls)
+  await browser.close();
+}
+
+function checkRequest (req) {
+  console.log(req.query)
+  const formid = req.query.formid
+  if (typeof formid !== 'string') return {
+    error: true,
+    message: 'Did not receive a valid formid'
+  }
+  if (!/^[a-z0-9\-]+$/i.test(formid)) return {
+    error: true,
+    message: 'formid contained invalid characters (only alphanumeral and - allowed)'
+  }
+  if (formid.length > 60 || formid.length < 50 ) return {
+    error: true,
+    message: 'formid was of invalid length'
+  }
+  return 'valid'
 }
