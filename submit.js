dd363715c16ae588157f0603770b61b6d0534ab1
Bart Grosman <grosman.bart@gmail.com>
2019-08-09 15:37:06 +0200

    changed sleep lcation

--- submit.js
+++ submit.js
@@ -34,11 +34,12 @@ module.exports = async (req, res) => {
     })
     // Filled all the inputs in their respective order, now submit
     document.querySelector('form').submit()
-    return new Promise(resolve => setTimeout(resolve, 1000)) // Wait one second so the form submit always goes through
   }, inputs)
-
-  await browser.close()
-  res.json({ success: true })
+  setTimeout(() => {
+    // Wait one second so the form submit always goes through
+    await browser.close()
+    res.json({ success: true })
+  }, 1000) 
 }
 
 function checkRequest (req) {
